30472f66200f591aae80c4d471081f932af9583e
qianyb <qianyb@ctrip.com>
2018-12-29 17:17:37 +0800

    State入门学习，顺带复习下Props

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-// props: name --- required
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowText: true,
+      showText: 0
+    };
+
+    setInterval(() => {
+      this.setState( previousState => { 
+        return { 
+          isShowText: !previousState.isShowText, 
+          showText: previousState.showText + 0.5
+        }
+      });
+    }, 500);
+  }
+
   render () {
-    return (
-      <Text>Hello, {this.props.name}</Text>
-    );
+    if (!this.state.isShowText) {
+      return null;
+    }else {
+      return (
+        <View>
+          <Text>{this.props.text}</Text>
+          <Text>{this.state.showText}</Text>
+        </View>
+      );
+    }
   }
 }
 
-export default class LogsOfGreeting extends Component {
+export default class BlinkApp extends Component {
   render () {
     return (
       <View style={style.container}>
-        <Greeting name='qianyb'/>
-        <Greeting name='yangtq'/>
+        <Blink text="1111111111"/>
+        <Blink text="2222222222"/>
       </View>
     );
   }
 }
+
 
 const style = StyleSheet.create({
   // flex布局知识点： https://www.cnblogs.com/xuyuntao/articles/6391728.html
