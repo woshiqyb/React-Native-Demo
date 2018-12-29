65292f1c3398478e88f344ac30bc4f78711934ab
qianyb <qianyb@ctrip.com>
2018-12-29 22:51:38 +0800

    处理触摸事件2之自定义按钮

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,40 +1,63 @@
 import React, { Component } from 'react';
-import { Button, Alert, StyleSheet, View } from 'react-native';
+import { Platform, Alert, View, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native';
 
-export default class ButtonBasics extends Component {
-  onPressButton() {
-    Alert.alert('你点击了Button！')
+export default class Touchables extends Component {
+  _onPressButton() {
+    Alert.alert('我点击了Button!')
   }
 
-  render() {
-    return(
+  _onLongPressButton() {
+    Alert.alert('我长按了Button!')
+  }
+
+  render() {  
+    return (
       <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button onPress={this.onPressButton} title='点击我'/>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button onPress={this.onPressButton} title="点击我" color="#841584" />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button onPress={this.onPressButton} title="This looks great!" />
-          <Button onPress={this.onPressButton} title="OK!" color="#841584" />
-        </View>
+      {/* underlayColor 是按下去透出来的颜色，用来做点击效果*/}
+        <TouchableHighlight onPress={this._onPressButton} underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={this._onPressButton} activeOpacity={0.8} >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableNativeFeedback onPress={this._onPressButton} background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+          </View>
+        </TouchableNativeFeedback>
+        {/* 长按手势的优先级高于点击手势，delayLongPress=0时，点击会被识别为长按 */}
+        <TouchableWithoutFeedback onPress={this._onPressButton} onLongPress={this._onLongPressButton} delayLongPress={500}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+          </View>
+        </TouchableHighlight>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    paddingTop: 60,
+    alignItems: 'center'
   },
-  buttonContainer: {
-    margin: 20
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
   },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+  buttonText: {
+    padding: 20,
+    color: 'white'
   }
 });
