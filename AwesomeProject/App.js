c8a0ecae09985f77d365d77edc04e1b32f58ebb5
qianyb <qianyb@ctrip.com>
2018-12-30 20:05:44 +0800

    入门基础终极篇之电影列表

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,58 +1,95 @@
+
 import React, { Component } from 'react';
-import { FlatList, ActivityIndicator, Alert, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, ActivityIndicator, FlatList, Alert } from 'react-native';
 
-export default class FetchExample extends Component {
+const REQUEST_URL = "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
 
-  constructor(props) {
+class TailText extends Component {
+  render() {
+    return (
+      <Text numberOfLines={3} style={this.props.style}>{this.props.text}</Text>
+    );
+  }
+}
+export default class MovieList extends Component {
+  constructor(props){
     super(props);
-    this.state = { isLoading: true, dataSource: [] };
+    this.state = {
+      isLoading: true,
+      movies: []
+    }
   }
 
   componentWillMount() {
-    // fetch('https://facebook.github.io/react-native/movies.json')
-    Promise.race([
-      fetch('https://facebook.github.io/react-native/movies.json'),
-      new Promise((_, reject) => {
-        setTimeout(() => {
-          reject();
-        }, 1000);
-      })
-    ])
-    .then((response) => {
-      return response.json();
-    })
+    fetch(REQUEST_URL)
+    .then((response) => response.json())
     .then((responseJson) => {
       this.setState({
         isLoading: false,
-        dataSource: responseJson.movies
+        movies: responseJson.movies
       });
     })
-    .catch( error => {
-      this.setState({
-        isLoading: false,
-        dataSource: []
-      });
-    });
+    .catch((error) => console.log(error))
   }
 
   render() {
     if (this.state.isLoading) {
       return (
-        <View style={{flex: 1, padding: 30, backgroundColor: 'red'}}>
-          <ActivityIndicator />
+        <View style={styles.container}>
+          <ActivityIndicator size='large'/>
+          <Text>正在加载电影数据...</Text>
         </View>
       );
     } else {
       return (
-        <View style={{flex: 1, padding: 30, backgroundColor: 'white'}}>
-          <FlatList 
-            data={this.state.dataSource} 
-            renderItem={({item}) => 
-              <Text>{item.title}, {item.releaseYear}</Text>
-            }
-            keyExtractor={(item, i) => item.id}/>
-        </View>
+        <FlatList 
+          style={styles.list} 
+          data={this.state.movies} 
+          renderItem={({item, index}) => 
+            <View style={[styles.container, index==0?{paddingTop:30}:(index==this.state.movies.length-1?{paddingBottom:30}:{})]}>
+              <Image style={[styles.thumbnail, {borderColor: 'blue', borderWidth: 1}]} source={{uri:item.posters.thumbnail}}/>
+              <View style={{flex:1}}>
+                <TailText style={styles.title} text={"电影名称："+item.title} />
+                <TailText style={styles.year} text={"上映年份："+item.year} />
+              </View>
+            </View>
+          } 
+          keyExtractor={(item,_) => item.id}/>
       );
     }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#F5FCFF'
+  },
+  thumbnail: {
+    width: 53,
+    height: 80
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    height: 50
+  },
+  list: {
+    // paddingTop: 40,
+    // padding: 40,
+    backgroundColor: '#F5FCFF'
+  }
+});
+
+const MOCKED_MOVIES_DATA = [
+  {
+    title: '标题',
+    year: '2015',
+    posters: {
+      thumbnail: 'https://i.imgur.com/UePbdph.jpg'
+    }
+  }
+];
