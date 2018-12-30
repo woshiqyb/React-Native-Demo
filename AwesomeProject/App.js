9e7162007f15b00cc80f7c8a61f14c3f13000c82
qianyb <qianyb@ctrip.com>
2018-12-30 16:55:54 +0800

    网络

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,56 +1,58 @@
 import React, { Component } from 'react';
-import { FlatList, Alert, SectionList, View, StyleSheet, Text } from 'react-native';
+import { FlatList, ActivityIndicator, Alert, Text, View } from 'react-native';
 
-export default class FlatListBasics extends Component {
+export default class FetchExample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true, dataSource: [] };
+  }
+
+  componentWillMount() {
+    // fetch('https://facebook.github.io/react-native/movies.json')
+    Promise.race([
+      fetch('https://facebook.github.io/react-native/movies.json'),
+      new Promise((_, reject) => {
+        setTimeout(() => {
+          reject();
+        }, 1000);
+      })
+    ])
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson.movies
+      });
+    })
+    .catch( error => {
+      this.setState({
+        isLoading: false,
+        dataSource: []
+      });
+    });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={{backgroundColor:'red', marginTop: 30, color:'white'}}>FlatList</Text>
-        <FlatList 
-          data={
-            [{key: 'Devin'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'}]} 
-            // 此处需要使用'{item}'解构拿到参数，
-            // 因为renderItem传入的实际是一个对象{item: ItemT;index: number;separators: {};}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>} />
-        <Text style={{backgroundColor:'red', marginTop: 30, color:'white'}}>SectionList</Text>
-        <SectionList 
-          sections={
-            [ {title: 'D', data: ['Devin']},
-              {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']}
-            ]
-          } 
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>} 
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(_, index) => index}/>
-      </View>
-    );
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, padding: 30, backgroundColor: 'red'}}>
+          <ActivityIndicator />
+        </View>
+      );
+    } else {
+      return (
+        <View style={{flex: 1, padding: 30, backgroundColor: 'white'}}>
+          <FlatList 
+            data={this.state.dataSource} 
+            renderItem={({item}) => 
+              <Text>{item.title}, {item.releaseYear}</Text>
+            }
+            keyExtractor={(item, i) => item.id}/>
+        </View>
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   paddingTop: 22
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  }
-});
