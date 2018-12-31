47d1f44dbe14f0d334dfde50eade075247c0b372
qianyb <qianyb@ctrip.com>
2019-01-01 00:07:24 +0800

    改善用户体验之TextInput、hitslop、KeyboardAvoidingView； 此示例中KeyboardAvoidingView在iphoneX模拟器上效果跟预期不一样，待修复

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,51 +1,60 @@
 import React, { Component } from 'react';
-import { NativeModules, LayoutAnimation, Text, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { Text, StatusBar, TouchableOpacity, View, TextInput, Button, Alert, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
-const { UIManager } = NativeModules;
-
-UIManager.setLayoutAnimationEnabledExperimental 
-&& UIManager.setLayoutAnimationEnabledExperimental(true);
-
-const INITIAL_W = 100;
-const INITIAL_H = 100;
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      w: INITIAL_W,
-      h: INITIAL_H
-    };
-    this._onPress = this._onPress.bind(this);
   }
 
-  _onPress() {
-    LayoutAnimation.spring();
-    this.setState((prevState) => ({
-      w: prevState.w + 15,
-      h: prevState.h + 15
-    }));
+  _submit () {
+    Alert.alert('haha');
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={[styles.box, {width: this.state.w, height: this.state.h}]} />
-        <TouchableOpacity onPress={this._onPress}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Press me!</Text>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.header}>
+          <Text style={styles.description}>
+            改善用戶体验的demo
+          </Text>
+        </View>
+        <View style={styles.content}>
+          <TouchableOpacity onPress={()=>{
+            Alert.alert('Without hitslop');
+          }}>
+          <Text style={styles.label}>Without hitSlop</Text>
+          </TouchableOpacity>
+          <View style={styles.separator} />
+          <TouchableOpacity 
+          hitSlop={{top: 20, left: 20, bottom: 20, right: 20}}                    onPress={() => {
+            Alert.alert('With hitslop');
+          }}>
+            <Text style={styles.label}>With hitSlop</Text>
+          </TouchableOpacity>
+          <KeyboardAvoidingView behavior="padding" style={styles.form}>
+          <TextInput
+            style={styles.input}
+            value={"this.state.email"}
+            placeholder="email@example.com"
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="email-address"
+            returnKeyType="send"
+            onSubmitEditing={this._submit}
+            blurOnSubmit={true}
+          />
+          <View>
+            <Button title="Sign Up" onPress={this._submit} />
+            <Text style={styles.legal}>
+              Some important legal fine print here
+              Some important legal fine print here
+              Some important legal fine print here
+              Some important legal fine print here
+            </Text>
           </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          LayoutAnimation.easeInEaseOut();
-          this.setState({
-            w: INITIAL_W,
-            h: INITIAL_H
-          });
-        }}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Reset style!</Text>
-          </View>
-        </TouchableOpacity>
+        </KeyboardAvoidingView>
+        </View>
       </View>
     );
   }
@@ -54,24 +63,50 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#ecf0f1',
+  },
+  header: {
+    paddingTop: 44,
+    padding: 20,
+    backgroundColor: '#336699',
+  },
+  description: {
+    fontSize: 14,
+    color: 'white',
+  },
+  content: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    width: 200,
-    height: 200,
-    backgroundColor: 'red',
+  label: {
+    fontSize: 18,
+    color: '#336699',
+    textAlign: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1,
   },
-  button: {
-    backgroundColor: 'black',
-    width: 100,
-    height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 15,
+  separator: {
+    height: 30,
   },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+  input: {
+    margin: 20,
+    marginBottom: 0,
+    height: 34,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    fontSize: 16,
+  },
+  legal: {
+    margin: 10,
+    color: '#333',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'space-between',
   },
 });
